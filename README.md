943a355b462a15cf28f90c5fff42a9a38e90f681
Asfakur <saikat.hap@gmail.com>
2021-09-12 12:59:10 +0600

    post method done

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # amader-bajar-backend
-# amader-bajar-backend
-# amader-bajar-backend
-# amader-bajar-backend
+To initialize the app
+$ npm init --yes
+
+to install express
+
+$ npm i express
+
+
+###it is used for auto reaload node server... and to run server in nodemon mode command=> nodemon index.js
+$ npm i -g nodemon
+
+
+app.use(express.json()); //it must be needed to enable parsing for getting data from post body
+
+$ npm install joi
+### Joi(https://www.npmjs.com/package/joi) is used to validate input information in server side
+
+
+const Joi = require('joi'); //its return a class from joi module so Joi is starts with upper case
--- index.js
+++ index.js
@@ -1,9 +1,55 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json()); //it must be needed to enable parsing for getting data from post body
+
+const Joi = require('joi'); //its return a class from joi module so Joi is starts with upper case
+
+
+const products = [
+    { id: 1, name: "Mango" },
+    { id: 2, name: "Banana" },
+    { id: 3, name: "Apple" },
+    { id: 4, name: "Lichi" }
+];
+
 app.get('/', (req, res) => {
     res.send("Welcome to amader bajar backend server");
 })
+
+app.get('/api/products', (req, res) => {
+    res.send(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(p => p.id === parseInt(req.params.id));
+    if (!product) return res.status(404).send("The product with the given Id is not found");
+
+    res.send(product);
+});
+
+app.post('/api/products', (req, res) => {
+    const { error } = validateProduct(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const product = {
+        id: products.length + 1,
+        name: req.body.name
+    };
+
+    products.push(product);
+    res.send(product);
+});
+
+function validateProduct(product) {
+    const schema = Joi.object({
+        name: Joi.string()
+            .min(3)
+            .required()
+    });
+    return schema.validate(product);
+}
+
 
 const port = process.env.PORT || 5000;
 
